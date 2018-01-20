68fc3bd7e2597a4d7eec42c366a8bb26dfce9422
Mjun4code <Mjun4code@outlook.com>
2018-01-20 13:30:53 +0800

    1005

---
+++ PAT/Advanced/1005.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string nums[] = { "zero","one","two","three","four","five","six","seven","eight","nine" };
+int main()
+{
+  string n;
+  cin >> n;
+  int sum = 0;
+  int len = n.length();
+  for (int i = 0; i < len; i++)
+    sum += n[i] - '0';
+  string s = to_string(sum);
+  cout << nums[s[0]-'0'];
+  len = s.length();
+  for (int i = 1; i < len; i++)
+    cout << " " << nums[s[i]-'0'];
+  return 0;
+}
